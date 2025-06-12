0344f5ed350d5748ce172bbf0d76d167095a50e6
ronitrajfr <ronitrajofficial7@gmail.com>
2025-06-12 10:07:15 +0530

    feat: make route to fetch all members

---
+++ src/app/api/members/route.ts
@@ -0,0 +1,79 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { string, z } from "zod";
+import { auth } from "~/server/auth";
+import { db } from "~/server/db";
+import { Ratelimit } from "@upstash/ratelimit";
+import { headers } from "next/headers";
+import { redis } from "~/utils/redis";
+
+const getReqRateLimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.slidingWindow(60, "10 s"),
+  analytics: true,
+});
+
+const membersGetSchema = z.object({
+  id: string(),
+});
+
+export async function GET(req: NextRequest) {
+  try {
+    const headersList = await headers();
+    const ip = headersList.get("x-forwarded-for") ?? "anonymous";
+    const { success, limit, reset, remaining } =
+      await getReqRateLimit.limit(ip);
+
+    if (!success) {
+      return NextResponse.json(
+        {
+          error: "Too many requests",
+          limit,
+          reset,
+          remaining,
+        },
+        {
+          status: 429,
+          headers: {
+            "X-RateLimit-Limit": limit.toString(),
+            "X-RateLimit-Remaining": remaining.toString(),
+            "X-RateLimit-Reset": reset.toString(),
+          },
+        },
+      );
+    }
+
+    const session = await auth();
+    if (!session?.user) {
+      return NextResponse.json({ error: "User unauthorized" }, { status: 401 });
+    }
+
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id");
+
+    const { id: validatedId } = membersGetSchema.parse({ id });
+
+    const cacheKey = `subConfetti:members:${validatedId}`;
+    const cached = await redis.get<string>(cacheKey);
+    if (cached) {
+      const members = typeof cached === "string" ? JSON.parse(cached) : cached;
+
+      return NextResponse.json({ members, cached: true }, { status: 200 });
+    }
+
+    const members = await db.member.findMany({
+      where: {
+        subConfettiChannelId: validatedId,
+      },
+    });
+
+    await redis.set(cacheKey, JSON.stringify(members), { ex: 300 });
+
+    return NextResponse.json({ members, cached: false }, { status: 200 });
+  } catch (error) {
+    console.error("Fetch members error:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
+  }
+}
