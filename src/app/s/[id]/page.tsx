d3dd77abdf0c294648ec3603ce40ba2c32b0bade
ronitrajfr <ronitrajofficial7@gmail.com>
2025-06-13 11:43:39 +0530

    check if user is part of that channel

--- src/app/s/[id]/page.tsx
+++ src/app/s/[id]/page.tsx
@@ -1,11 +1,18 @@
 import { DiscordSidebar } from "~/components/members-sidebar";
-
+import { checkMembership } from "~/actions/check-membership";
 export default async function page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
+
+  const { isMember } = await checkMembership(id);
+  console.log(isMember);
+
+  if (!isMember) {
+    return <div>you are not fucking part of this server</div>;
+  }
 
   return (
     <div className="=">
